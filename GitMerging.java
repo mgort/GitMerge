debb22e60d764cfad1097caa09e9c210bf2f58d3
danielmawhirter <dmawhirt@mines.edu>
2014-02-21 18:17:17 -0700

    Rework main to include feedback

--- GitMerging.java
+++ GitMerging.java
@@ -1,7 +1,19 @@
 
 public class GitMerging {
-
-	public static void main(String [] args) {
-		
+	
+	public void feedback(int score) {
+		if (score == 100)
+			System.out.println("You're awesome");
+		else if (score > 90)
+			System.out.println("That's great");
+		else if (score > 60)
+			System.out.println("That's good ");
+		else
+			System.out.println("Well, what can I say?");
+	}
+	
+	public static void main(String[] args) {
+		GitMerging tm = new GitMerging();
+		tm.feedback(60);
 	}
 }
